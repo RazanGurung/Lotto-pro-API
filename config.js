366398b1ec75ca1875bb71ffc54b9d2088685cc4
BishalJung <bishaljung63@gmail.com>
2025-12-04 13:47:38 -0500

    config.js changed

--- config.js
+++ config.js
@@ -1,21 +1,19 @@
+// config.js
 require('dotenv').config();
 const mysql = require('mysql2');
 
-// Build DB connection string
 const urlDB =
   `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}` +
   `@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}` +
   `/${process.env.MYSQLDATABASE}`;
 
-// Create MySQL connection
 const connection = mysql.createConnection(urlDB);
 
-// Connect
 connection.connect((err) => {
   if (err) {
     console.error('❌ Database connection failed:', err);
   } else {
-    console.log('✅ Connected to Railway MySQL Database!');
+    console.log('✅ Connected to MySQL Database!');
   }
 });
 
